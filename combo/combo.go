7f00a3669c017c1069c8ba69631dea709e9480c1
Dmitry Chestnykh <dmitry@codingrobots.com>
2013-09-21 17:55:19 +0200

    Initial commit.

---
+++ combo/combo.go
@@ -0,0 +1,117 @@
+// Package combo implements an http.Handler that serves concatenated files in a single request.
+package combo
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+type Handler struct {
+	// Root file system.
+	Root http.FileSystem
+
+	// URL path (unrestricted if empty).
+	URLPath string
+
+	// Separator of file names in request query (e.g. "&").
+	Separator string
+
+	// Maximum number of files to concatenate.
+	// If more requested, returns an Bad Request error.
+	MaxFiles int
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h.URLPath != "" && h.URLPath != r.URL.Path {
+		http.NotFound(w, r)
+		return
+	}
+	filenames := strings.Split(r.URL.RawQuery, h.Separator)
+	if len(filenames) == 0 {
+		http.NotFound(w, r)
+		return
+	}
+	if len(filenames) > h.MaxFiles {
+		http.Error(w, fmt.Sprintf("Too many files: %d", len(filenames)), http.StatusBadRequest)
+		return
+	}
+	// Unescape names.
+	for i, escapedName := range filenames {
+		name, err := url.QueryUnescape(escapedName)
+		if err != nil {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
+		filenames[i] = name
+	}
+	if !stringsUnique(filenames) {
+		http.Error(w, "Request contains repeated name(s)", http.StatusBadRequest)
+		return
+	}
+	h.serveFiles(w, r, filenames)
+}
+
+func stringsUnique(strs []string) bool {
+	set := make(map[string]struct{}, len(strs))
+	for _, s := range strs {
+		if _, ok := set[s]; ok {
+			return false
+		}
+		set[s] = struct{}{}
+	}
+	return true
+}
+
+func (h *Handler) serveFiles(w http.ResponseWriter, r *http.Request, filenames []string) {
+	var buf bytes.Buffer
+	var maxModTime time.Time
+	for _, name := range filenames {
+		err := h.appendFileContent(&buf, &maxModTime, name)
+		if err != nil && os.IsNotExist(err) {
+			http.Error(w, fmt.Sprintf("File not found: %q", name), http.StatusNotFound)
+			return
+		}
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+	}
+	http.ServeContent(w, r, filenames[0], maxModTime, bytes.NewReader(buf.Bytes()))
+}
+
+func (h *Handler) appendFileContent(buf *bytes.Buffer, maxModTime *time.Time, filename string) error {
+	// Open file.
+	f, err := h.Root.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// Check that file is not a directory.
+	fi, err := f.Stat()
+	if err != nil {
+		return err
+	}
+	if fi.IsDir() {
+		// We do not serve directories, so it's "not found" error.
+		return os.ErrNotExist
+	}
+
+	// Update max modification time.
+	t := fi.ModTime()
+	if t.After(*maxModTime) {
+		*maxModTime = t
+	}
+
+	// Add file contents to the buffer.
+	if _, err := io.Copy(buf, f); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ comboserver/main.go
@@ -0,0 +1,74 @@
+// Comboserver serves multiple files combined in a single request.
+//
+//   Usage: comboserver <directory>
+//     -addr="localhost:8080": address to serve content from
+//     -maxfiles=50: maximum files to concatenate in a single request
+//     -root="/": root URL
+//     -sep="&": file list separator
+// 
+// Example:
+//
+//  $ comboserver -addr=localhost:8081 -root=/combo /var/www
+//
+// This will launch a web server serving files from /var/www. Suppose it
+// contains the following files:
+//
+//     base.css
+//     pure/grids.css
+//     pure/buttons.css
+//     
+// Then these files, concatenated, are available with a single request:
+//
+//     http://localhost:8081/combo?base.css&pure/grids.css&pure/buttons.css
+//
+// Files can be combined in any way:
+//
+//     http://localhost:8081/combo?pure/buttons.css&base.css
+//
+// The program preserves the order of request, doesn't allow repeated
+// filenames, and limits the number of files to 50 by default. Content-type
+// header of the reponse is set to the type of the first file in list.
+//
+// Comboserver doesn't do any caching, as it's a job of whatever nginx you put
+// in front of it.
+//
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/dchest/comboserver/combo"
+)
+
+var (
+	fAddr     = flag.String("addr", "localhost:8080", "address to serve content from")
+	fRoot     = flag.String("root", "/", "root URL")
+	fSep      = flag.String("sep", "&", "file list separator")
+	fMaxFiles = flag.Int("maxfiles", 50, "maximum files to concatenate in a single request")
+)
+
+func main() {
+	flag.Parse()
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s <directory>\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	if flag.NArg() < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	http.Handle(*fRoot, &combo.Handler{
+		Root:      http.Dir(flag.Arg(0)),
+		URLPath:   *fRoot,
+		Separator: *fSep,
+		MaxFiles:  *fMaxFiles,
+	})
+
+	fmt.Printf("serving combo files from %s at %s\n", flag.Arg(0), *fAddr)
+	log.Fatal(http.ListenAndServe(*fAddr, nil))
+}
